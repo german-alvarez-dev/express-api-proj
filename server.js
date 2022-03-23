27992769ec501aee9b6e247aa2a41715af271510
german-alvarez-dev <german.alvarez@ironhack.com>
2022-03-23 15:48:21 +0100

    port

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ app.use(cors())
 
 app.use("/", require('./routes/products.routes'))
 
-app.listen("5005", () => console.log("Server started at: http://localhost:5005"))
+const PORT = process.env.PORT || 5005
+app.listen(PORT, () => console.log("Server started at: http://localhost:5005"))
